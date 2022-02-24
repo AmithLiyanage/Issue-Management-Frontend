e3c4e4d31cac9176571160818f6325883ac25be7
Amith Liyanage <amith.liyanage.9@gmail.com>
2022-02-24 19:14:06 +0530

    piechart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import AppBar from './components/Appbar';
 import Issue from './components/Issue';
+import PieChart from './components/PieChart';
 
 function App() {
     return ( 
     <div className = "App" >
         <AppBar/>
+        <PieChart/>
         <Issue/>
         </div>);
     }
