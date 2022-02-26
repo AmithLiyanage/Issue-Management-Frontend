55eba7e29215a77dee79e5c5af5a2b69a3ce9358
Amith Liyanage <amith.liyanage.9@gmail.com>
2022-02-27 00:50:59 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import IssuePieChart from './components/IssuePieChart';
 
 ReactDOM.render(
     <App />,
+    <IssuePieChart/>,
 
   // <React.StrictMode>
   //   <App />
