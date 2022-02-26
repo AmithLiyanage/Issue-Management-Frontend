4fc080bdaa6e6b4247b016c61a37e1f25f38e179
Amith Liyanage <amith.liyanage.9@gmail.com>
2022-02-27 00:58:30 +0530

    update pie chart

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -20,15 +20,18 @@ export default function Issue() {
 
     return (
         <Container>
-            <h2>All Issues</h2>
             <Paper elevation={3} style={paperStyle}>
+                <h2>All Issues</h2>
                 {issues.map(issue => (
-                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={issue.issueId}>
-                        issueId:{issue.issueId}<br/>
-                        IssueName:{issue.issueName}<br/>
-                        Description:{issue.description}<br/>
-                        Type:{issue.type}<br/>
-                        State:{issue.state}<br/>
+                    <Paper elevation={6} style={{ margin: "8px", padding: "15px", textAlign: "left" }} key={issue.issueId}>
+                        <div style={{ display: 'flex', marginBottom: "10px" }}>
+                            <div style={{ width: "50%", textAlign: "left" }}>#{issue.issueId}   {issue.issueName}</div>
+                            <div style={{ width: "50%", textAlign: "end" }}>{issue.type}</div>
+                        </div>
+                        <div style={{ display: 'flex'}}>
+                            <div style={{ width: "60%", textAlign: "left" }}>{issue.description}</div>
+                            <div style={{ width: "40%", textAlign: "end" }}>{issue.state}</div>
+                        </div>
                     </Paper>
                 ))}
             </Paper>
